07a8eff381cd849c83ec9cb1277f60470896cfbc
Rafael <rafaelgonzag4@gmail.com>
2025-01-02 11:43:41 -0300

    add Evento Controller

---
+++ controllers/eventoController.ts
@@ -0,0 +1,36 @@
+import Evento from "../models/Evento"
+import { Request, Response } from 'express'
+
+export const getEventos = async (req: Request, res: Response): Promise<void>  => {
+    try {
+      const eventos = await Evento.findAll();
+      res.status(200).json(eventos);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Erro ao buscar grupos '});
+    }
+};
+
+export const createGrupo = async (req: Request, res: Response): Promise<void> => {
+  const { descricao, tipo, data, user_id_dst }  = req.body;
+
+  try {
+      const evento = await Evento.create({ descricao, tipo, data, user_id_dst });
+      res.status(201).json(evento);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Erro ao criar grupo' });
+  }
+}
+
+export const getGrupoById = async (req: Request, res: Response): Promise<void> => {
+  const id = req.params.id;
+
+  try {
+    const evento = await Evento.findByPk(id);
+    res.status(200).json(evento);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao buscar grupo '});
+  }
+}
